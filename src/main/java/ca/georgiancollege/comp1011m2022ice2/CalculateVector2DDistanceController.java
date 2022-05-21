e71c547979e2515f6b2c71ea7d7cfc67a7fc6caf
Nitin <73417686+nitin4679@users.noreply.github.com>
2022-05-21 12:46:40 -0400

    Added Project Files

---
+++ src/main/java/ca/georgiancollege/comp1011m2022ice2/CalculateVector2DDistanceController.java
@@ -0,0 +1,6 @@
+package ca.georgiancollege.comp1011m2022ice2;
+
+public class CalculateVector2DDistanceController
+{
+
+}
---
+++ src/main/java/ca/georgiancollege/comp1011m2022ice2/HelloController.java
@@ -0,0 +1,14 @@
+package ca.georgiancollege.comp1011m2022ice2;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
